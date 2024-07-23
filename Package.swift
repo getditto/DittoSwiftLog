4ad6083bf7126204141a8db8abe091f7cbd3af00
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2024-07-23 14:10:19 -0400

    Update versions to align with DittoSwiftPackage 4.8.0-rc.1

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DittoSwiftLog",
-    platforms: [ .iOS(.v11), .macOS(.v11) ],
+    platforms: [ .iOS(.v14), .macOS(.v11), .macCatalyst(.v14), .tvOS(.v14) ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
