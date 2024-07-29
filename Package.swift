2e4eb82e98b197e7b3431d6334717a9f0f63f5bb
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2024-07-29 09:07:44 -0400

    Update README, fix iOS min version to align to Ditto SDK, pass messages directly instead of adding data (consumers should use Logger metadata in swift-log instead)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DittoSwiftLog",
-    platforms: [ .iOS(.v11), .macOS(.v11) ],
+    platforms: [ .iOS(.v14), .macOS(.v11) ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
